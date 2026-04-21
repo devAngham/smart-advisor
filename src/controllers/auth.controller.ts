9c3422075fa986d68cf49fe5f1f4cb7641e30134
dev Angham <anghamaabed95@gmail.com>
2026-04-21 19:26:57 +0300

    feat: implement auth endpoints (register & login)

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from 'express'
-import { compare } from 'bcryptjs'
+import bcrypt, { compare } from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import prisma from '../config/prisma'
 
-import { ApiResponse, LoginBody } from '../types'
+import { ApiResponse, LoginBody, RegisterBody } from '../types'
 
 
 export const login = async (req: Request<{}, {}, LoginBody>, res: Response) : Promise<void> => {
@@ -73,3 +73,63 @@ export const login = async (req: Request<{}, {}, LoginBody>, res: Response) : Pr
         } as ApiResponse<null>)
     }
 }
+
+export const register = async (req: Request<{}, {}, RegisterBody>, res: Response) => {
+  try {
+
+    const { email, name, password } = req.body;
+    // Validate required fields
+    if (!email || !name || !password) {
+      res.status(400).json({
+        success: false,
+        message: 'All fields are required',
+        data: null
+      } as ApiResponse<null>)
+      return
+    }
+
+    // check if user already exist
+    const isUser = await prisma.user.findUnique({ where: { email } })
+
+    if (isUser) {
+      res.status(409).json({
+        success: false,
+        message: 'Email is already exist',
+        data: null
+      } as ApiResponse<null>)
+      return
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await prisma.user.create({ data: { email, name, password: hashedPassword }});
+    // Generate token
+    const token = jwt.sign(
+      {userId: user.id, },
+      process.env.JWT_SECRET as string,
+      { expiresIn: '7d' }
+    )
+
+    res.status(200).json({
+      success: true,
+      message: `Welcome ${name}`,
+      data: {
+        token,
+        user: {
+          id: user.id,
+          name: user.name,
+          email: user.email
+        }
+      }
+      // token: string, {user: { id: number, email: string, userId: string, }}}
+    } as ApiResponse<{ token: string; user: { id: number, email: string, name: string }}>)
+
+  } catch(err) {
+    console.log('[Auth] Register error:', err)
+    res.status(500).json({
+      success: false,
+      message: 'Internal server error',
+      data: null
+    } as ApiResponse<null>)
+  }
+}
+
